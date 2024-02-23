7f2a84356559b8eb27d33c016a301f0fc855a32e
Philip Tempelman <tempelmanphilip@gmail.com>
2024-02-22 21:47:38 -0500

    Change website title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Head>
-        <title>Clubadvisor</title>
+        <title>ActiveNYC</title>
         <meta name="description" content="💭" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
