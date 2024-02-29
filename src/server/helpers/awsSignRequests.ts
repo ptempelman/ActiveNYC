fc128e4a5b3b8fc23c54edc97d9a9c649978ba1f
Philip Tempelman <tempelmanphilip@gmail.com>
2024-02-29 15:16:09 -0500

    add request signing with smithy instead of aws sdk

--- src/server/helpers/awsSignRequests.ts
+++ src/server/helpers/awsSignRequests.ts
@@ -1,30 +1,50 @@
 
-import { AwsClient } from 'aws4fetch';
 
-import * as crypto from 'crypto';
+import { Sha256 } from "@aws-crypto/sha256-browser";
+import { HttpRequest } from "@smithy/protocol-http";
+import { SignatureV4 } from "@smithy/signature-v4";
 
-export async function signRequest(method: string, url: string, body?: any): Promise<Response> {
+import fetch from 'node-fetch';
 
-    const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-    if (!accessKeyId || !secretAccessKey) {
-        throw new Error("AWS credentials not found");
+export async function signRequest<T = unknown>(method: string, url: string, payload: T | undefined = undefined) {
+    const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
+    const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+    const region = process.env.AWS_REGION;
+
+    if (!awsAccessKeyId || !awsSecretAccessKey || !region) {
+        throw new Error('AWS credentials not found');
     }
 
-    const aws = new AwsClient({
-        accessKeyId: accessKeyId,
-        secretAccessKey: secretAccessKey,
-    });
+    const endpoint = new URL(url);
 
-    const headers = {
-        'Content-Type': 'application/json',
-    };
-
-    const response = await aws.fetch(url, {
+    const request = new HttpRequest({
         method: method,
-        headers: body ? headers : {},
-        body: body ? JSON.stringify(body) : undefined,
+        protocol: endpoint.protocol,
+        hostname: endpoint.hostname,
+        path: endpoint.pathname,
+        headers: {
+            host: endpoint.host,
+            accept: 'application/json',
+            'content-type': 'application/json',
+        },
+        body: payload !== undefined ? JSON.stringify(payload) : undefined,
+    })
+
+    const signer = new SignatureV4({
+        credentials: {
+            accessKeyId: awsAccessKeyId,
+            secretAccessKey: awsSecretAccessKey,
+        },
+        service: 'execute-api',
+        region: region,
+        sha256: Sha256,
     });
 
-    return response;
+    const signedRequest = await signer.sign(request);
+
+    return fetch(url, {
+        method: signedRequest.method,
+        headers: signedRequest.headers,
+        body: signedRequest.body as string,
+    });
 }
